6747ed21fb1b0998e22554897a9ad08c7dab4713
Alexandr Mansurov <alex@eghuro.cz>
2021-09-08 20:54:56 +0200

    Remove linting from main app

--- tsa/commands.py
+++ tsa/commands.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 """Click commands."""
 import os
-from glob import glob
-from subprocess import call
 
 import click
 from flask import current_app
@@ -15,45 +13,12 @@ TEST_PATH = os.path.join(PROJECT_ROOT, 'tests')
 
 
 @click.command()
-def test():
-    """Run the tests."""
-    import pytest
-    rv = pytest.main([TEST_PATH, '--verbose'])
-    exit(rv)
-
-
-@click.command()
-@click.option('-f', '--fix-imports', default=False, is_flag=True,
-              help='Fix imports using isort, before linting')
-def lint(fix_imports):
-    """Lint and check code style with flake8 and isort."""
-    skip = ['node_modules', 'requirements', 'venv', 'migrations', 'venv-new', 'venv-test']
-    root_files = glob('*.py')
-    root_directories = [
-        name for name in next(os.walk('.'))[1] if not name.startswith('.')]
-    files_and_directories = [
-        arg for arg in root_files + root_directories if arg not in skip]
-
-    def execute_tool(description, *args):
-        """Execute a checking tool with its arguments."""
-        command_line = list(args) + files_and_directories
-        click.echo('{}: {}'.format(description, ' '.join(command_line)))
-        rv = call(command_line)
-        if rv != 0:
-            exit(rv)
-
-    if fix_imports:
-        execute_tool('Fixing import order', 'isort', '-rc')
-    execute_tool('Checking code style', 'flake8')
-
-
-@click.command()
 def clean():
     """Remove *.pyc and *.pyo files recursively starting at current directory.
 
     Borrowed from Flask-Script, converted to use Click.
     """
-    for dirpath, dirnames, filenames in os.walk('.'):
+    for dirpath, _, filenames in os.walk('.'):
         for filename in filenames:
             if filename.endswith('.pyc') or filename.endswith('.pyo'):
                 full_pathname = os.path.join(dirpath, filename)
