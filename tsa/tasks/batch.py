e8ed1e231a8c45ee7baed5e54f868a6df186ce79
Alexandr Mansurov <alex@eghuro.cz>
2021-09-11 02:00:10 +0200

    Refactor batch tasks: complexity, typing

--- tsa/tasks/batch.py
+++ tsa/tasks/batch.py
@@ -1,7 +1,9 @@
 """Celery tasks for batch processing of endpoiint or DCAT catalog."""
 import logging
+from typing import Any, Collection, Generator, Iterable, List, Tuple
 
 import rdflib
+import redis
 from celery import group
 from rdflib import Graph, Namespace
 from rdflib.namespace import RDF
@@ -19,7 +21,7 @@ from tsa.tasks.process import filter_iri, process, process_priority
 from tsa.util import test_iri
 
 
-def query_parent(dataset_iri, endpoint):
+def _query_parent(dataset_iri: str, endpoint: str) -> Generator[str, None, None]:
     opts = [f'<{dataset_iri!s}> <http://purl.org/dc/terms/isPartOf> ?parent',
             f'?parent <http://purl.org/dc/terms/hasPart> <{dataset_iri!s}> ',
             f'<{dataset_iri!s}> <http://www.w3.org/ns/dcat#inSeries> ?parent',
@@ -32,97 +34,118 @@ def query_parent(dataset_iri, endpoint):
             yield str(parent_iri)
 
 
-def _dcat_extractor(graph, red, log, force, graph_iri, lookup_endpoint):
-    distributions, distributions_priority = [], []
-    dcat = Namespace('http://www.w3.org/ns/dcat#')
-    dcterms = Namespace('http://purl.org/dc/terms/')
-    nkod = Namespace('https://data.gov.cz/slovník/nkod/mediaTyp')
-    media_priority = set([
-        'https://www.iana.org/assignments/media-types/application/rdf+xml',
-        'https://www.iana.org/assignments/media-types/application/trig',
-        'https://www.iana.org/assignments/media-types/text/n3',
-        'https://www.iana.org/assignments/media-types/application/ld+json',
-        'https://www.iana.org/assignments/media-types/application/n-triples',
-        'https://www.iana.org/assignments/media-types/application/n-quads',
-        'https://www.iana.org/assignments/media-types/text/turtle'
-    ])  # IANA
-    format_priority = set([
-        'http://publications.europa.eu/resource/authority/file-type/RDF',
-        'http://publications.europa.eu/resource/authority/file-type/RDFA',
-        'http://publications.europa.eu/resource/authority/file-type/RDF_N_QUADS',
-        'http://publications.europa.eu/resource/authority/file-type/RDF_N_TRIPLES',
-        'http://publications.europa.eu/resource/authority/file-type/RDF_TRIG',
-        'http://publications.europa.eu/resource/authority/file-type/RDF_TURTLE',
-        'http://publications.europa.eu/resource/authority/file-type/RDF_XML',
-        'http://publications.europa.eu/resource/authority/file-type/JSON_LD',
-        'http://publications.europa.eu/resource/authority/file-type/N3'
-    ])  # EU
-    queue = distributions
+dcat = Namespace('http://www.w3.org/ns/dcat#')
+dcterms = Namespace('http://purl.org/dc/terms/')
+nkod = Namespace('https://data.gov.cz/slovník/nkod/mediaTyp')
+media_priority = set([
+    'https://www.iana.org/assignments/media-types/application/rdf+xml',
+    'https://www.iana.org/assignments/media-types/application/trig',
+    'https://www.iana.org/assignments/media-types/text/n3',
+    'https://www.iana.org/assignments/media-types/application/ld+json',
+    'https://www.iana.org/assignments/media-types/application/n-triples',
+    'https://www.iana.org/assignments/media-types/application/n-quads',
+    'https://www.iana.org/assignments/media-types/text/turtle'
+])  # IANA
+format_priority = set([
+    'http://publications.europa.eu/resource/authority/file-type/RDF',
+    'http://publications.europa.eu/resource/authority/file-type/RDFA',
+    'http://publications.europa.eu/resource/authority/file-type/RDF_N_QUADS',
+    'http://publications.europa.eu/resource/authority/file-type/RDF_N_TRIPLES',
+    'http://publications.europa.eu/resource/authority/file-type/RDF_TRIG',
+    'http://publications.europa.eu/resource/authority/file-type/RDF_TURTLE',
+    'http://publications.europa.eu/resource/authority/file-type/RDF_XML',
+    'http://publications.europa.eu/resource/authority/file-type/JSON_LD',
+    'http://publications.europa.eu/resource/authority/file-type/N3'
+])  # EU
+dsdistr, distrds = ds_distr()
+distributions: List[str] = []
+distributions_priority: List[str] = []
 
+
+def _get_queue(distribution: Any, graph: rdflib.Graph) -> List[str]:
+    queue = distributions
+    # put RDF distributions into a priority queue
+    for media in graph.objects(distribution, dcat.mediaType):
+        if str(media) in media_priority:
+            queue = distributions_priority
+
+    for distribution_format in graph.objects(distribution, dcterms.format):
+        if str(distribution_format) in format_priority:
+            queue = distributions_priority
+
+    # data.gov.cz specific
+    for distribution_format in graph.objects(distribution, nkod.mediaType):
+        if 'rdf' in str(distribution_format):
+            queue = distributions_priority
+    return queue
+
+
+def _distribution_extractor(distribution: Any, dataset: Any, effective_dataset: Any, graph: rdflib.Graph, pipe: redis.client.Pipeline, log: logging.Logger) -> Tuple[set[str], List]:
+    log.debug(f'Distr: {distribution!s}')
+    queue = _get_queue(distribution, graph)
+
+    # download URL to files
+    downloads = []
+    endpoints = set()
+    for download_url in graph.objects(distribution, dcat.downloadURL):
+        # log.debug(f'Down: {download_url!s}')
+        if test_iri(str(download_url)) and not filter_iri(str(download_url)):
+            if download_url.endswith('/sparql'):
+                log.info(f'Guessing {download_url} is a SPARQL endpoint, will use for dereferences from DCAT dataset {dataset!s} (effective: {effective_dataset!s})')
+                endpoints.add(download_url)
+            else:
+                downloads.append(download_url)
+                distribution = True
+                log.debug(f'Distribution {download_url!s} from DCAT dataset {dataset!s} (effective: {effective_dataset!s})')
+                queue.append(download_url)
+                pipe.sadd(f'{dsdistr}:{str(effective_dataset)}', str(download_url))
+                pipe.sadd(f'{distrds}:{str(download_url)}', str(effective_dataset))
+        else:
+            log.debug(f'{download_url!s} is not a valid download URL')
+
+    # scan for DCAT2 data services here as well
+    for access in graph.objects(distribution, dcat.accessService):
+        log.debug(f'Service: {access!s}')
+        for endpoint in graph.objects(access, dcat.endpointURL):
+            if test_iri(str(endpoint)):
+                log.debug(f'Endpoint {endpoint!s} from DCAT dataset {dataset!s}')
+                endpoints.add(endpoint)
+    return endpoints, downloads
+
+
+def _dataset_extractor(dataset: Any, lookup_endpoint: str, graph: rdflib.Graph, log: logging.Logger, pipe: redis.client.Pipeline) -> bool:
+    log.debug(f'DS: {dataset!s}')
+    effective_dataset = dataset
+    distribution = False
+
+    for parent in _query_parent(dataset, lookup_endpoint):
+        log.debug(f'{parent!s} is a series containing {dataset!s}')
+        effective_dataset = parent
+
+    # DCAT Distribution
+    endpoints, downloads = set(), []
+    for distribution in graph.objects(dataset, dcat.distribution):
+        local_endpoints, local_downloads = _distribution_extractor(distribution, dataset, effective_dataset, graph, pipe, log)
+        endpoints.update(local_endpoints)
+        downloads.extend(local_downloads)
+    for endpoint in endpoints:
+        pipe.sadd(dataset_endpoint(str(effective_dataset)), endpoint)
+
+    if not downloads and endpoints:
+        log.warning(f'Only endpoint without distribution for {dataset!s}')
+
+    return distribution
+
+
+def _dcat_extractor(graph: rdflib.Graph, red: redis.Redis, log: logging.Logger, force: bool, graph_iri: str, lookup_endpoint: str) -> None:
     log.debug(f'Extracting distributions from {graph_iri}')
     # DCAT dataset
     with TimedBlock('dcat_extractor'):
-        dsdistr, distrds = ds_distr()
         distribution = False
         with red.pipeline() as pipe:
             for dataset in graph.subjects(RDF.type, dcat.Dataset):
-                log.debug(f'DS: {dataset!s}')
-                effective_dataset = dataset
-
-                for parent in query_parent(dataset, lookup_endpoint):
-                    log.debug(f'{parent!s} is a series containing {dataset!s}')
-                    effective_dataset = parent
-
-                # DCAT Distribution
-                for distribution in graph.objects(dataset, dcat.distribution):
-                    log.debug(f'Distr: {distribution!s}')
-                    # put RDF distributions into a priority queue
-                    for media in graph.objects(distribution, dcat.mediaType):
-                        if str(media) in media_priority:
-                            queue = distributions_priority
-
-                    for distribution_format in graph.objects(distribution, dcterms.format):
-                        if str(distribution_format) in format_priority:
-                            queue = distributions_priority
-
-                    # data.gov.cz specific
-                    for distribution_format in graph.objects(distribution, nkod.mediaType):
-                        if 'rdf' in str(distribution_format):
-                            queue = distributions_priority
-
-                    # download URL to files
-                    downloads = []
-                    endpoints = set()
-                    for download_url in graph.objects(distribution, dcat.downloadURL):
-                        # log.debug(f'Down: {download_url!s}')
-                        if test_iri(str(download_url)) and not filter_iri(str(download_url)):
-                            if download_url.endswith('/sparql'):
-                                log.info(f'Guessing {download_url} is a SPARQL endpoint, will use for dereferences from DCAT dataset {dataset!s} (effective: {effective_dataset!s})')
-                                endpoints.add(download_url)
-                            else:
-                                downloads.append(download_url)
-                                distribution = True
-                                log.debug(f'Distribution {download_url!s} from DCAT dataset {dataset!s} (effective: {effective_dataset!s})')
-                                queue.append(download_url)
-                                pipe.sadd(f'{dsdistr}:{str(effective_dataset)}', str(download_url))
-                                pipe.sadd(f'{distrds}:{str(download_url)}', str(effective_dataset))
-                        else:
-                            log.debug(f'{download_url!s} is not a valid download URL')
-
-                    # scan for DCAT2 data services here as well
-                    for access in graph.objects(distribution, dcat.accessService):
-                        log.debug(f'Service: {access!s}')
-                        for endpoint in graph.objects(access, dcat.endpointURL):
-                            if test_iri(str(endpoint)):
-                                log.debug(f'Endpoint {endpoint!s} from DCAT dataset {dataset!s}')
-                                endpoints.add(endpoint)
-
-                    for endpoint in endpoints:
-                        pipe.sadd(dataset_endpoint(str(effective_dataset)), endpoint)
-
-                    if not downloads and endpoints:
-                        log.warning(f'Only endpoint without distribution for {dataset!s}')
-
+                distribution_local = _dataset_extractor(dataset, lookup_endpoint, graph, log, pipe)
+                distribution = distribution or distribution_local
             pipe.execute()
     # TODO: possibly scan for service description as well
         if distribution:
@@ -151,12 +174,12 @@ def _dcat_extractor(graph, red, log, force, graph_iri, lookup_endpoint):
 
 
 @celery.task(base=TrackableTask)
-def inspect_graph(endpoint_iri, graph_iri, force):
+def inspect_graph(endpoint_iri: str, graph_iri: str, force: bool) -> None:
     red = inspect_graph.redis
-    return do_inspect_graph(graph_iri, force, red, endpoint_iri)
+    _do_inspect_graph(graph_iri, force, red, endpoint_iri)
 
 
-def do_inspect_graph(graph_iri, force, red, endpoint_iri):
+def _do_inspect_graph(graph_iri: str, force: bool, red: redis.Redis, endpoint_iri: str) -> None:
     log = logging.getLogger(__name__)
     try:
         inspector = SparqlEndpointAnalyzer(endpoint_iri)
@@ -167,25 +190,25 @@ def do_inspect_graph(graph_iri, force, red, endpoint_iri):
 
 
 @celery.task
-def inspect_graphs(graphs, endpoint_iri, force):
+def inspect_graphs(graphs: Iterable[str], endpoint_iri: str, force: bool) -> None:
     red = inspect_graphs.redis
     for graph in graphs:
-        do_inspect_graph(graph, force, red, endpoint_iri)
+        _do_inspect_graph(graph, force, red, endpoint_iri)
 
 
-def multiply(item, times):
+def _multiply(item: Any, times: int):
     for _ in range(times):
         yield item
 
 
-def split(a, n):
+def _split(a, n):
     k, m = divmod(len(a), n)
     return (a[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n))
 
 
 @celery.task(base=TrackableTask)
-def batch_inspect(endpoint_iri, graphs, force, chunks):
+def batch_inspect(endpoint_iri: str, graphs: Collection[str], force: bool, chunks: int) -> celery.result.AsyncResult:
     items = len(graphs)
     monitor.log_graph_count(items)
     logging.getLogger(__name__).info(f'Batch of {items} graphs in {endpoint_iri}')
-    return inspect_graph.chunks(zip(multiply(endpoint_iri, items), graphs, multiply(force, items)), chunks).apply_async()
+    return inspect_graph.chunks(zip(_multiply(endpoint_iri, items), graphs, _multiply(force, items)), chunks).apply_async()
