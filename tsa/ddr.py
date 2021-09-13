e4e56595ab8420cf8b498837380192c256d2c1c4
Alexandr Mansurov <alex@eghuro.cz>
2021-09-13 09:17:07 +0200

    Reduce logging and test for collection length in dsd index

--- tsa/ddr.py
+++ tsa/ddr.py
@@ -16,10 +16,10 @@ class DataDrivenRelationshipIndex:
     def index(self, relationship_type: str, iri1: str, iri2: str) -> None:
         log = logging.getLogger(__name__)
         if not rfc3987.match(iri1) or not iri1.startswith('http'):
-            log.error('Not an iri: %s (relationship_type: %s)', iri1, relationship_type)
+            log.debug('Not an iri: %s (relationship_type: %s)', iri1, relationship_type)
             return
         if not rfc3987.match(iri2) or not iri2.startswith('http'):
-            log.error('Not an iri: %s (relationship_type: %s)', iri2, relationship_type)
+            log.debug('Not an iri: %s (relationship_type: %s)', iri2, relationship_type)
             return
 
         with self.__red.pipeline() as pipe:
@@ -68,7 +68,8 @@ class DataCubeDefinitionIndex:
             # pipe.sadd('dsd:rod', iri)
             for dataset in dsd:
                 for dimension in dataset['dimensions']:
-                    pipe.sadd(f'dsd:rod:{iri}', *dimension['resources'])
+                    if len(dimension['resources']) > 0:
+                        pipe.sadd(f'dsd:rod:{iri}', *dimension['resources'])
             pipe.execute()
 
     def resources_on_dimension(self) -> Generator[Tuple[str, str], None, None]:
