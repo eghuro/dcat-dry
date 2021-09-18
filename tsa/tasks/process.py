5dcc51069c085bd2c0bde386a74433ed0e0128d3
Alexandr Mansurov <alex@eghuro.cz>
2021-09-18 19:57:37 +0200

    Update filter - more exclusions

--- tsa/tasks/process.py
+++ tsa/tasks/process.py
@@ -43,6 +43,7 @@ def filter_iri(iri):
     return iri.endswith('csv.zip') or iri.endswith('csv') or iri.endswith('csv.gz') or iri.endswith('xls') or \
         iri.endswith('docx') or iri.endswith('xlsx') or iri.endswith('pdf') or \
         (iri.startswith('https://isdv.upv.cz') and iri.endswith('zip')) or  \
+        iri.startswith('http://geoportal.gov.cz') or iri.startswith('https://gis.brno.cz') or \
         ((iri.startswith('http://vdp.cuzk.cz') or iri.startswith('https://vdp.cuzk.cz')) and (iri.endswith('xml.zip') or iri.endswith('xml'))) or \
         ((iri.startswith('http://dataor.justice.cz') or iri.startswith('https://dataor.justice.cz')) and (iri.endswith('xml') or iri.endswith('xml.gz'))) or \
         iri.startswith('https://apl.czso.cz/iSMS/cisexp.jsp') or iri.startswith('https://eagri.cz') or \
