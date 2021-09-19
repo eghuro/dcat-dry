6c64646796a60a96505f5bbfa5a13ba2cdc14b6a
Alexandr Mansurov <alex@eghuro.cz>
2021-09-19 17:17:57 +0200

    Refactor rdf jsonld test

--- tests/test_rdf.py
+++ tests/test_rdf.py
@@ -1,6 +1,9 @@
 import rdflib
+import requests
 
 
 def test_jsonld():
+    r = requests.get('https://data.mpsv.cz/od/soubory/ciselniky/formy-soc-sluzby-ofn.jsonld')
+    data = r.text
     g = rdflib.Graph()
-    g.parse("https://data.mpsv.cz/od/soubory/ciselniky/formy-soc-sluzby-ofn.jsonld")
+    g.parse(data=data, format="json-ld")
